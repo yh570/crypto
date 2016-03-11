57cd18274afa0c498dc2c9bf8065c73fd3972661
JOkerlin <anonymous.joker.lin@gmail.com>
2016-03-10 19:40:35 -0500

    do some test

--- enc.py
+++ enc.py
@@ -3,6 +3,7 @@ import random
 DEBUG = 1
 
 L = 500
+WORDS_NUM = 100
 KEY_LENGTH = {'a': 8, 'b': 1, 'c': 3, 'd': 4, 'e': 13, 'f': 2,'g': 2,
               'h': 6, 'i': 7,'j': 1, 'k': 1, 'l': 4,'m': 2, 'n': 7,
               'o': 8,'p': 2, 'q': 1, 'r': 6,'s': 6, 't': 9,
@@ -28,9 +29,10 @@ def test_2_plaintext_gen():
   plaintext = ""
   f = open('english_words.txt')
   english_words = f.readlines()
-  random_index = random.sample(xrange(len(english_words)), len(english_words))
+  print english_words
+  random_index = random.sample(xrange(len(english_words)), WORDS_NUM)
 
-  for i in range(L):
+  for i in range(WORDS_NUM):
     plaintext += english_words[random_index[i]][:-2] + " "
   # delete last space-separate
   return plaintext[:-1]
@@ -54,8 +56,7 @@ def enc_test_1(plaintext):
   return ciphertext
 
 # return tuple of plaintext and ciphertext
-def enc_test_2():
-  plaintext = test_2_plaintext_gen()
+def enc_test_2(plaintext):
   ciphertext = ""
   cipher_maps = enc_cipher_maps_gen()
 
@@ -67,11 +68,12 @@ def enc_test_2():
     if j < len(plaintext) - 1:
       if plaintext[j + 1] != " ":
         ciphertext += ","
-  return plaintext, ciphertext
+  return ciphertext
 
 if __name__ == "__main__":
   #plaintext_test = "revelation revering rightest impersonalize juliennes scientists reemphasizing propose crony bald pampering discharged lincoln authoresses interacted laked bedmaker intolerably beltlines warningly worldliness serologic bottom guessed hangup vitiates snaky polypous manifolding sweatshirt divisiveness decapitation musketry versers pizzas aperies reorganizes fender presentations thereuntil fly entrapped causewayed shaped freemasonry nudging efflorescence hydrated zazen exegeses fracas unprogressivel"
   #print enc_test_1(plaintext_test)
   #print test_2_plaintext_gen()
-  print enc_test_2()[0]
-  print enc_test_2()[1]
+  # print enc_test_2(plaintext = test_2_plaintext_gen())[0]
+  # print enc_test_2(plaintext = test_2_plaintext_gen())[1]
+  print test_2_plaintext_gen()
