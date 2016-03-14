d08da2b7a3bb7fbecbac40ccc39af3d055bc8377
JOkerlin <anonymous.joker.lin@gmail.com>
2016-03-14 00:51:35 -0400

    add final zip

--- setup.py
+++
@@ -1,8 +0,0 @@
-from os import chdir,path
-from sys import argv
-from distutils.core import setup
-import py2exe
-chdir(path.dirname(argv[0]))
-setup_file=argv.pop()
-argv.append('py2exe')
-setup(console=[setup_file])
